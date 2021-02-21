feafd7f16624278c7eeaf81bbb99805f4318f962
michal <zendran.m@gmail.com>
2021-02-21 22:17:23 +0100

    Profile images styling

--- src/app/Passengers.ts
+++ src/app/Passengers.ts
@@ -20,9 +20,41 @@ export interface Passenger {
 export const Passengers: Array<Passenger> = [
   {
     name: 'Chandler',
-    photo: 'image1',
+    photo: 'assets/chandler.jpg',
     interactions: 4,
     email: 'duckandchick@gmail.com',
+    phone: '123 456 789',
+    location: 'New York, NY',
+    route: {
+      start: 'A',
+      end: 'B',
+      distance: 123,
+      time: '12h',
+      price: '100 euro',
+      energy: '12.4 kWh',
+    },
+  },
+  {
+    name: 'Monica',
+    photo: 'assets/monica.jpg',
+    interactions: 4,
+    email: 'monana@gmail.com',
+    phone: '123 456 789',
+    location: 'New York, NY',
+    route: {
+      start: 'A',
+      end: 'B',
+      distance: 123,
+      time: '12h',
+      price: '100 euro',
+      energy: '12.4 kWh',
+    },
+  },
+  {
+    name: 'Joey',
+    photo: 'assets/joey.jpg',
+    interactions: 4,
+    email: 'ilovepizza@gmail.com',
     phone: '123 456 789',
     location: 'New York, NY',
     route: {
